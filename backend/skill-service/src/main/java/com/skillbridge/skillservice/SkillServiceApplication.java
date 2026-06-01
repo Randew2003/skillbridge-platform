8f30d3cd0f0284195de35b342136d04da822b3ee
R M weerathunga <randewmegha98@gmail.com>
2026-06-02 02:50:53 +0530

    Format main method in SkillServiceApplication for consistency

--- backend/skill-service/src/main/java/com/skillbridge/skillservice/SkillServiceApplication.java
+++ backend/skill-service/src/main/java/com/skillbridge/skillservice/SkillServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkillServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SkillServiceApplication.class, args);
+    }
 }
