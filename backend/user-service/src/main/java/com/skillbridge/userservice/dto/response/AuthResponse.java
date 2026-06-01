6a1873fbb4853c551d03584b050403936782bb1e
R M weerathunga <randewmegha98@gmail.com>
2026-06-01 17:21:56 +0530

    Add AuthResponse DTO for user authentication response

---
+++ backend/user-service/src/main/java/com/skillbridge/userservice/dto/response/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.skillbridge.userservice.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String token;
+    private String tokenType;
+    private Long userId;
+    private String fullName;
+    private String email;
+    private String role;
+}
