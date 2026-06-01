90523d626588a400ea12a3ae30e9bc959921ee10
R M weerathunga <randewmegha98@gmail.com>
2026-06-01 23:29:50 +0530

    Add ProjectService interface to define project management operations

---
+++ backend/project-service/src/main/java/com/skillbridge/projectservice/service/ProjectService.java
@@ -0,0 +1,23 @@
+package com.skillbridge.projectservice.service;
+
+import com.skillbridge.projectservice.dto.request.ProjectRequest;
+import com.skillbridge.projectservice.dto.response.ProjectResponse;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectResponse createProject(ProjectRequest request);
+
+    ProjectResponse getProjectById(Long id);
+
+    List<ProjectResponse> getAllProjects();
+
+    List<ProjectResponse> getProjectsByOwner(Long ownerId);
+
+    List<ProjectResponse> getOpenProjects();
+
+    ProjectResponse updateProject(Long id, ProjectRequest request);
+
+    void deleteProject(Long id);
+}
