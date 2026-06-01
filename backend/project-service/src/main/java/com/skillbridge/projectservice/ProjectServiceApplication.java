c67860ae054064ae1125cc42aed8d3c8994c80b8
R M weerathunga <randewmegha98@gmail.com>
2026-06-01 22:52:50 +0530

    Enable Feign clients in ProjectServiceApplication

--- backend/project-service/src/main/java/com/skillbridge/projectservice/ProjectServiceApplication.java
+++ backend/project-service/src/main/java/com/skillbridge/projectservice/ProjectServiceApplication.java
@@ -2,12 +2,13 @@ package com.skillbridge.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProjectServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServiceApplication.class, args);
+    }
 }
