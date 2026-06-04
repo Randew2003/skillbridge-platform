6296bbf3edf16aa7be01c722f27d0836108aeafa
R M weerathunga <randewmegha98@gmail.com>
2026-06-05 00:28:05 +0530

    feat: add constants for API configuration and application settings

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "http://localhost:8080";
+
+export const APP_NAME = "SkillBridge";
+
+export const TOKEN_KEY = "skillbridge_token";
+
+export const USER_KEY = "skillbridge_user";
